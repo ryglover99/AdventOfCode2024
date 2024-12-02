48ab1d74b6f014f8c3b7423426e77df28245cae5
ryglover99 <ryanglover123@gmail.com>
2024-12-02 20:06:45 +0000

    Add project files.

---
+++ AdventOfCode2024/Day 1/Day1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.Day_1
+{
+    public class Day1
+    {
+        public (List<int>, List<int>) ParseInputFileIntoTwoLists(string location)
+        {
+            if (!File.Exists(location))
+            {
+                return (new List<int>(), new List<int>());
+            }
+
+            string input = File.ReadAllText(location);
+
+            List<int> list1 = new List<int>();
+            List<int> list2 = new List<int>();
+
+            var split = input.Split("\r\n");
+            
+            foreach (string line in split)
+            {
+                var nums = line.Split("   ");
+                list1.Add(Int32.Parse(nums[0]));
+                list2.Add(Int32.Parse(nums[1]));
+            }
+
+            return (list1, list2);
+        }
+
+
+        public int FindLowestNumberInList(List<int> list)
+        {
+
+            list.Sort((a, b) => b.CompareTo(a));
+
+            return list.Last();
+        }
+
+    }
+}
---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,58 @@
+﻿
+using AdventOfCode2024.Day_1;
+
+var day1 = new Day1();
+
+var (list1,list2) = day1.ParseInputFileIntoTwoLists("C:\\Users\\ryang\\Desktop\\Github Projects\\AdventOfCode2024\\AdventOfCode2024\\Day 1\\input.txt");
+
+
+List<int> differenceBetweenLowest = new List<int>();
+List<int> similarScore = new List<int>();
+
+foreach (var num in list1)
+{
+    var all = list2.FindAll(x => x == num);
+
+    var simScore = num * all.Count();
+    similarScore.Add(simScore);
+}
+
+foreach (var num in list1.ToList())
+{
+
+    var lowest1 = day1.FindLowestNumberInList(list1);
+
+
+    foreach (var num2 in list2.ToList())
+    {
+
+        var lowest2 = day1.FindLowestNumberInList(list2);
+
+        if (lowest1 > lowest2)
+        {
+            differenceBetweenLowest.Add(lowest1 - lowest2);
+
+        } else if (lowest2 > lowest1)
+        {
+            differenceBetweenLowest.Add(lowest2 - lowest1);
+
+        } else if (lowest1 == lowest2)
+        {
+            differenceBetweenLowest.Add(0);
+        }
+
+        list1.Remove(lowest1);
+        list2.Remove(lowest2);
+        break;
+
+    }
+}
+
+var finalAnswer = differenceBetweenLowest.Sum();
+var simScoreSum = similarScore.Sum();
+
+Console.WriteLine($"THE FINAL ANSWER IS: {finalAnswer}");
+
+
+
+
